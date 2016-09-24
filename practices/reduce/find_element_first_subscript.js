bf4efbedf28460dba03c48059288f53670eb5a56
wangpan <lucky.com>
2016-09-24 23:16:15 +0800

    practices/reduce/find_element_first_subscript.js

--- practices/reduce/find_element_first_subscript.js
+++ practices/reduce/find_element_first_subscript.js
@@ -2,9 +2,7 @@
 
 function calculate_elements_sum(collection, element) {
   //在这里写入代码
-  return collection.reduce(function (element1,i,collection) {
-   return collection.indexOf(element)===i
-  })
+  return collection.indexOf(element);
 }
 
 module.exports = calculate_elements_sum;
