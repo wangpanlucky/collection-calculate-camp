7feeb9c3c5dc6bb9cd2446d60e05c4c89387702d
wangpan <lucky.com>
2016-09-24 23:47:31 +0800

    finish find_last_even.js

--- practices/reduce/find_last_even.js
+++ practices/reduce/find_last_even.js
@@ -2,6 +2,10 @@
 
 function find_last_even(collection) {
   //在这里写入代码
+  collection.reverse();
+  return collection.find(function (temp) {
+      return temp % 2 === 0;
+  });
 }
 
 module.exports = find_last_even;
