06bc15e0dcea109a7b8d25a50b54f1750a4fdca9
wangpan <lucky.com>
2016-09-24 22:43:50 +0800

    finish practices/reduce/compute_chain_median

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -2,8 +2,19 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
-
-
+  let arr1 = collection.split('->');
+  let arr3 = arr1.map(function (temp) {
+    return parseInt(temp);
+  }).sort(function (a, b) {
+    return a - b;
+  });
+  let t = arr3.length / 2;
+  if (arr3.length % 2 === 0) {
+    return (arr3[t] + arr3[t - 1]) / 2;
+  }
+  else {
+    return arr3[t];
+  }
 }
 
 module.exports = compute_chain_median;
