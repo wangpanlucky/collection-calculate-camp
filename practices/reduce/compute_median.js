7d3d6894efc01a0060cd35e7c9ac588bb430004b
wangpan <lucky.com>
2016-09-24 23:02:54 +0800

    practices/reduce/compute_median.js

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -2,6 +2,16 @@
 
 function compute_median(collection) {
   //在这里写入代码
+  let arr = collection.sort(function (a, b) {
+    return a - b;
+  });
+  let t = Math.floor(arr.length / 2);
+  if (arr.length % 2 === 0) {
+    return (arr[t] + arr[t - 1]) / 2;
+  }
+  else {
+    return arr[t];
+  }
 }
 
 module.exports = compute_median;
