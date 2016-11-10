fcad621fae8a7d614a43bdc7bf085f0141e71841
wangpan <lucky.com>
2016-11-10 20:16:02 +0800

    finish section_2/practice_1_spec.js

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,21 @@
+
+/*global module*/
 function count_same_elements(collection) {
   //在这里写入代码
+  let obj = {};
+  let result = [];
+  let key;
+  let noRepeat = collection.filter(function (elem, index, collection) {
+    if (collection.indexOf(elem) === index) {
+      return elem;
+    }
+  });
+  noRepeat.map(function (elem) {
+    key = elem;
+    obj[key] = collection.lastIndexOf(elem) - collection.indexOf(elem) + 1;
+    result.push(Object.assign({key: key, count: obj[key]}));
+  });
+  return result;
 }
 
 module.exports = count_same_elements;
