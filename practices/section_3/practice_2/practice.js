c43e019ec2cfe499b84825b177570ce26c5cf1ff
wangpan <lucky.com>
2016-11-10 21:16:16 +0800

    finished practices/section_3/practice_2/practice.js

--- practices/section_3/practice_2/practice.js
+++ practices/section_3/practice_2/practice.js
@@ -1,5 +1,20 @@
+/*global module*/
 function create_updated_collection(collection_a, object_b) {
   //在这里写入代码
+
+  let a, b;
+  a = collection_a.map(function (elem) {
+    return elem.key;
+  });
+  b = object_b.value;
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < b.length; j++) {
+      if (a[i] === b[j]) {
+        collection_a[i].count = collection_a[i].count - Math.floor(collection_a[i].count / 3);
+      }
+    }
+  }
+  return collection_a;
 }
 
 module.exports = create_updated_collection;
